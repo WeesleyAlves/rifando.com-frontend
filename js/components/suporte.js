818ebc7ecab021f555fb4438192cb005d6ccf9f3
Wesley <weealves1@gmail.com>
2020-04-03 12:22:45 -0300

    pagina de suporte finalizada

---
+++ js/components/suporte.js
@@ -0,0 +1,16 @@
+Vue.component('form-suporte',{
+    template: `
+    <div class="container-form">
+        <div class="container-buttons">
+            <a>Contato</a>
+        </div>
+
+        <form action="">
+            <input type="text" placeholder="Email para contato">
+            <input type="text" placeholder="Assunto">
+            <textarea name="" id="" cols="30" rows="10" placeholder="Mensagem"></textarea>
+            <button class='primary-btn'>Enviar</button>
+        </form>
+    </div>    
+    `
+});
